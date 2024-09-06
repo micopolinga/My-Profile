13a8956e3de8ba3fd5b4f01aae3129a726164437
micopolinga <mycopoli87@gmail.com>
2024-09-06 08:31:56 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Mico S. Polinga';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'I am a student with a passion for software development. I am learning to write, test, debug, and maintain software code to create functional applications and systems. I am building my skills in programming languages, algorithms, data structures, and various software development methods..';
+export const ABOUT_TEXT_2 =
+  'I am a student focused on developing and maintaining web applications, working on both the front end and back end. I collaborate with different teams to design, implement, and deploy scalable solutions that align with business goals.';
+// export const ABOUT_TEXT_3 =
+//   'Versatile professional proficient in both frontend and backend development, capable of building complete web applications from start to finish. Possess a strong understanding of various programming languages, frameworks, and databases.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'I am a student with a strong interest in software development, particularly in building websites and web applications. I am learning to use modern JavaScript tools and frameworks, and I am gaining experience with both front-end and back-end technologies as well as database management.';
+// export const RESUME_TEXT_2 =
+//   'Software Developer with 12+ years of experience building websites and web applications using modern JavaScript  tools/frameworks.';
